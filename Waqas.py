49a9568039d892c78423cb06b4a97c0bd8baf89e
Waqaso0 <122422151+Waqaso0@users.noreply.github.com>
2023-03-09 12:47:49 +0500

    Create Waqas.py

---
+++ Waqas.py
@@ -0,0 +1,56 @@
+<p align="left"> <img src="https://komarev.com/ghpvc/?username=cobra-404-cyber&label=Profile%20views&color=0e75b6&style=flat" alt="cobra-404-cyber" /> </p>
+
+<h1 align="center">FARHAN KHAN</h1>
+
+<a href="#"><img title="COBRA-404-CYBER" src="https://raw.githubusercontent.com/MRVIVEK-CODER/MRVIVEK-CODER/main/md7Oqrf.gif"></a>
+
+<h1 align="center">COBRA 404 CYBER</h1>
+
+<a href="#"><img title="COBRA-404-CYBER" src="https://k.top4top.io/p_2596pji1e0.jpg"></a>
+
+- contact me **shayan12afridi@gmail.com**
+
+<h3 align="center">A passionate frontend developer from Bangladesh</h3>
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗡𝗔𝗠𝗘 : SHAYAN AFRIDI
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 : [SHAYAN AFRIDI](https://www.facebook.com/profile.php?id=100079622233578)
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗣𝗔𝗚𝗘 : AFRIDIYAN 404](https://www.facebook.com/Cobra.404.Cyber/)
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗚𝗥𝗢𝗨𝗣 : [AFRIDIYAN 404 𝐂𝐘𝐁𝐄𝐄 𝐖𝐎𝐑𝐋𝐃](https://www.facebook.com/profile.php?id=100079622233578)
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 : [SHAYAN AFRIDI](https://wa.me/03136777415?text=)
+
+<img align="center" alt="line" src="https://github.com/DalpatRathore/dalpatrathore/blob/main/assets/images/line-2.svg">
+
+𝗧𝗘𝗟𝗜𝗚𝗥𝗔𝗠 : [SHAYAN AFRIDI](t.me/@AFRIDI4)
+
+<h3 align="left">Connect with me:</h3>
+
+<p align="left">
+
+<a href="https://www.facebook.com/virtua.jhonny.sins" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/facebook.svg" alt="farhan khan" height="30" width="40" /></a>
+
+<a href="https://instagram.com/farhan khan" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="farhan khan" height="30" width="40" /></a>
+
+<a href="https://youtube.com/@CobraTecBD" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/youtube.svg" alt="cobra te bd" height="30" width="40" /></a>
+
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+
+<p align="left"> <a href="https://www.linux.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg" alt="linux" width="40" height="40"/> </a> <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a> </p>
+
+<p>&nbsp;<img align="center" src="https://github-readme-stats.vercel.app/api?username=cobra-404-cyber&show_icons=true&locale=en" alt="cobra-404-cyber" /></p>
+
